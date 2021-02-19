6d65e6768257422d05c5659deacb5b9addeb4b95
Aidan Casey <aidancasey@tw-he-m-ac.home>
2021-02-19 22:08:23 +0000

    trim down toolbar.vue

--- src/components/core/Toolbar.vue
+++ src/components/core/Toolbar.vue
@@ -3,86 +3,21 @@
     dark
     app
     :color="$root.themeColor">
-    <v-toolbar-title>
+    <v-toolbar-title> SSSSSS
       <v-toolbar-side-icon @click="toggleNavigationBar"></v-toolbar-side-icon>
     </v-toolbar-title>
-
-    <v-menu class="toolbar-menu-item" offset-y origin="center center" :nudge-bottom="10" transition="scale-transition">
-      <v-list>
-        <v-list-tile
-          v-for="(item,index) in items"
-          :key="index"
-          :to="!item.href ? { name: item.name } : null"
-          :href="item.href"
-          ripple="ripple"
-          :disabled="item.disabled"
-          :target="item.target"
-          @click="item.click">
-          <v-list-tile-action v-if="item.icon">
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-content>
-            <v-list-tile-title>{{ item.title }}</v-list-tile-title>
-          </v-list-tile-content>
-        </v-list-tile>
-      </v-list>
-    </v-menu>
   </v-toolbar>
 </template>
 <script>
-
 export default {
   data() {
     return {
-      rating: null,
-      dialog: false,
-      dialogSettings: false,
-      switchEmailNotification: true,
-      showPassword: null,
-      showPasswordConfirm: null,
-      userEmail: null,
-      password: null,
-      passwordConfirm: null,
-      error: false,
-      showResult: false,
-      result: ''
     }
   },
-
   methods: {
     toggleNavigationBar() {
       const vm = this;
-
       vm.$emit('toggleNavigationBar');
-    },
-
-    setUpSettings() {
-      const vm = this;
-
-      if (vm.userEmail === null || vm.password === null || vm.passwordConfirm === null) {
-
-        vm.result = "Email and Password can't be null.";
-        vm.showResult = true;
-
-        return;
-      }
-
-      if (vm.password !== vm.passwordConfirm) {
-
-        vm.error = true;
-        vm.result = "Passwords does not match the confirm password.";
-        vm.showResult = true;
-
-        return;
-      }
-
-      vm.$root.userEmail = vm.userEmail;
-      vm.$root.userPassword = vm.password;
-
-      vm.result = "Email and password changed succesfully.";
-      vm.showResult = true;
-
-      vm.dialogSettings = false;
     },
   }
 };
@@ -120,6 +55,4 @@ export default {
     width: 235px;
     margin-right: 10px;
   }
-
-
 </style>
